d5a35fd9eac41657b650b223897be105995019c9
Mingotrees <jhanell.mingo@gmail.com>
2025-08-27 17:52:09 +0800

    feat: version 2 completely implemented

--- array-list-implementation/version-2/pointer-to-array.c
+++ array-list-implementation/version-2/pointer-to-array.c
@@ -27,7 +27,7 @@ typedef struct{
 }studDetails;
 
 typedef struct{
-    studDetails elements[MAX];
+    studDetails elements[MAX]; 
     int count;
 }LIST, *ListPtr;
 
@@ -35,7 +35,8 @@ typedef struct{
 //CRUD OPERATIONS
 void insert(ListPtr arr, studDetails student, int pos);
 void delete(ListPtr arr, int id);
-void locate(LIST arr);
+int locate(LIST arr, int id);
+void populate(ListPtr arr);
 studDetails retrieve(LIST arr, int id);
 void initList(ListPtr* arr);
 ListPtr initList2();
@@ -43,6 +44,7 @@ ListPtr initList2();
 
 //HELPER FUNCTIONS
 void displayStuds(LIST arr);
+void displayStud(studDetails details);
 void mallocFail(){
     printf("Malloc Failed\n");
 }
@@ -57,26 +59,39 @@ int main(){
     * USE THESE TO POPULATE
     */
     studDetails students[] = {
-    { {"Alice", "Santos", 'M'}, 1001, "BSCS", 1 },
-    { {"Brian", "Cruz", 'D'}, 1002, "BSIT", 2 },
-    { {"Carla", "Lopez", 'R'}, 1003, "BSEE", 3 },
     { {"David", "Reyes", 'J'}, 1004, "BSME", 4 },
     { {"Ella", "Garcia", 'L'}, 1005, "BSN", 1 }
     };
-    int size = sizeof(students) / sizeof(students[0]);
-
-
 
     ListPtr a = initList2();
-    initList(&a);
-    insert(a, students[1], 0);
-    printf("%d\n", a->count);
-    insert(a, students[1], 1);
-    printf("%d\n", a->count);
+    // initList(&a);
+    populate(a);
+    // insert(a, students[0], 0);
+    // insert(a, students[1], 1);
     displayStuds(*a);
+    int location = locate(*a, 1001);
+    printf("%d\n", location + 1);
+    studDetails details = retrieve(*a, 1001);
+    displayStud(details);
+    // delete(a, 1003);
+    // displayStuds(*a);
+    // delete(a, 1002);
+    // displayStuds(*a);
     free(a);
 }
 
+void populate(ListPtr arr){
+    studDetails students[] = {
+        { {"Alice", "Santos", 'M'}, 1001, "BSCS", 1 },
+        { {"Brian", "Cruz", 'D'}, 1002, "BSIT", 2 },    
+        { {"Carla", "Lopez", 'R'}, 1003, "BSEE", 3 },
+    };
+    int size = sizeof(students) / sizeof(students[0]);
+
+    for(int ndx = 0; ndx < size; ndx++){
+        insert(arr, students[ndx], ndx);
+    }
+}
 
 
 ListPtr initList2(){
@@ -98,7 +113,20 @@ void initList(ListPtr* arr){
     }
 }
 
+void displayStud(studDetails details){
+    printf("===== STUDENT %d=====\n", details.ID);
+    printf("Name: %s %c. %s | Course: %s | Year: %d\n" ,
+        details.name.FName,
+        details.name.Mi,
+        details.name.LName,
+        details.course,
+        details.Year
+    );
+}
+
+
 void displayStuds(LIST arr){
+    printf("===== STUDENTS REMAINING IN LIST: %d\n", arr.count);
     for (int i = 0; i < arr.count; i++) {
         printf("ID: %d | Name: %s %c. %s | Course: %s | Year: %d\n",
             arr.elements[i].ID,
@@ -125,7 +153,29 @@ void insert(ListPtr arr, studDetails student, int pos){
 }
 
 void delete(ListPtr arr, int id){
-    
+    int ndx;
+    for(ndx = 0; ndx < arr->count && arr->elements[ndx].ID != id; ndx++){}
+    if(ndx < arr->count){
+        arr->count--;
+        for(;ndx < arr->count; ndx++){
+            arr->elements[ndx] = arr->elements[ndx+1]; 
+        } 
+    }else{
+        printf("Elem not Found");
+    }
+}
+
+int locate(LIST arr, int id){
+    int ndx;
+    for(ndx = 0; ndx < arr.count && arr.elements[ndx].ID != id; ndx++){}
+    return ndx < arr.count ? ndx : -1;
+}
+
+studDetails retrieve(LIST arr, int ID){
+    int ndx;
+    studDetails dummy = {.ID = -1};
+    for(ndx = 0; ndx < arr.count && arr.elements[ndx].ID != ID; ndx++){}
+    return ndx < arr.count ? arr.elements[ndx] : dummy;
 }
 
 
