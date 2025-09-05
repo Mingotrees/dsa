d3cb5f5c29b9b727014f86b0572fb9f7bf0bf65a
Mingotrees <jhanell.mingo@gmail.com>
2025-09-05 21:42:21 +0800

    feat: added queues for linked-list-impl

---
+++ linked-list-implementation/queues/singlyQueue.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+}*LIST;
+
+typedef struct{
+    LIST beginning;
+    LIST end;
+}Queue;
+
+void initialize(Queue* B);
+void enqueue(Queue* B, int elem);
+void dequeue(Queue* B);
+int front(Queue B);
+bool isEmpty(Queue B);
+bool isFull(Queue B);
+void printQueue(Queue* B);
+
+int main(){
+    Queue A;
+    initialize(&A);
+    for(int ndx = 2; ndx < 5; ndx++){
+        enqueue(&A, ndx);
+    }
+    printQueue(&A);
+
+    for(int ndx = 0; ndx < 3; ndx++){
+        dequeue(&A);
+        printQueue(&A);
+    }
+}
+
+void initialize(Queue* B){
+    B->beginning = NULL;
+    B->end = NULL;
+}
+
+/*
+* This method adds an element at the end of
+* the queue
+* @param queue pass this by address
+* @param element depending on your structure pass an element
+* @return nothing cousinS
+*/
+void enqueue(Queue* B, int elem){
+    LIST temp = (LIST)malloc(sizeof(struct node));
+    temp->data = elem;
+    temp->next = NULL;
+    if(isEmpty(*B)){
+        B->beginning = temp;
+    }else{
+        B->end->next = temp;
+    }
+    B->end = temp;
+}
+
+void dequeue(Queue* B){
+    if(!isEmpty(*B)){
+        LIST temp = B->beginning;
+        B->beginning = temp->next;
+        free(temp);
+        if(B->beginning == NULL){
+            B->end = NULL;
+        }
+    }
+}
+
+int front(Queue B){
+    return isEmpty(B) != true ? B.beginning->data : -1;
+}
+
+bool isEmpty(Queue B){
+    return B.beginning == NULL ? true : false;
+}
+
+bool isFull(Queue B){
+    return false;
+}
+
+void printQueue(Queue* B){
+    if(!isEmpty(*B)){
+        // version 1
+        // int temp = B->beginning->data;
+        // printf("%d\n", temp);
+        // dequeue(B);
+        // enqueue(B, temp);
+        // for(;B->beginning->data != temp;){
+        //     int hold = B->beginning->data;
+        //     printf("%d\n", hold);
+        //     dequeue(B);
+        //     enqueue(B, hold);
+        // }
+
+        //version 2
+        int temp = front(*B);
+        do{
+            printf("%d\n", front(*B));
+            enqueue(B, front(*B));
+            dequeue(B);
+        }while(front(*B) != temp);
+
+        printf("\n");
+    }else{
+        printf("Queue Empty");
+    }
+}
---
+++ thisSomeBullshit.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#define MAGIC 100
+
+typedef struct{} zz;
+
+typedef struct{
+    zz* ZZ;
+}yy;
+
+typedef struct{
+    yy* YY;
+}xx;
+
+struct bruh{
+    xx* WW;
+};
+
+struct curry{
+    struct bruh* UU;
+    void* VV;
+};
+
+typedef struct{
+    struct curry* TT;
+}ss;
+
+typedef struct{
+    ss SS;
+}rr;
+
+typedef struct{
+    rr RR;
+}qq;
+
+typedef struct{
+    qq QQ;
+}pp;
+
+struct dust{
+    pp PP;
+};
+
+struct hell{
+    struct dust* OO;
+    void* NN;
+};
+
+typedef struct{
+    char S[MAGIC];
+    char T[MAGIC];
+    char U;
+    char LL;
+    struct hell* MM;
+}r;
+
+struct itsLike11Pm{
+    r R;
+};
+
+typedef struct{
+    char P[MAGIC];
+    struct itsLike11Pm* Q;
+}o;
+
+//FOR H.I HIERARCHY
+typedef struct{
+    char T[MAGIC];
+    char U[MAGIC];
+    char v;
+}s;
+
+typedef struct{
+    char X[MAGIC];
+    char y;
+}w;
+
+struct idkAnymore{
+    s S;
+    w W;
+};
+
+struct helloWorld{
+    char N;
+    struct idkAnymore* M;
+};
+
+typedef struct{
+    char J;
+    char K;
+    struct helloWorld* L;
+}i;
+
+//FOR H
+typedef struct{
+    i I;
+    o O;
+}h;
+
+struct bigBoy{
+    char F;
+    char G;
+    h H;
+};
+
+//checkpoint for B.D
+typedef struct{
+    struct bigBoy* E;
+}e;
+
+//for B.C
+typedef struct{}kk;
+typedef struct{
+    kk KK;
+}jj;
+typedef struct{
+    jj JJ;
+}ii;
+
+struct hmm{
+    ii II;
+};
+
+struct huh{
+    struct hmm* HH;
+};
+
+struct lol{
+    struct huh* GG;
+};
+
+struct damn{
+    struct lol* EE;
+    char FF;
+};
+
+typedef struct{
+    struct damn* DD;
+}cc;
+
+typedef struct{
+    cc BB;
+}bb;
+
+typedef struct{
+    bb AA;
+}z;
+
+struct bigBoy2{
+    z Z;
+};
+
+//B
+typedef struct{
+    e* D;
+    struct bigBoy2* C;
+}b;
+
+typedef struct{
+    b B;
+}A;
