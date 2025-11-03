36d2c4422eec691d6ac41294c3a6abe5b008b6d6
Mingotrees <jhanell.mingo@gmail.com>
2025-11-03 19:59:42 +0800

    feat: bruh why am i skipping things

---
+++ lastPassOrDie/finals/BST/array.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#define MAX 6
+
+//my attempt
+
+typedef struct{
+    int arr[MAX];
+    int count;
+}Tree;
+
+typedef enum {EMPTY = -1} TreeStatus;
+
+void display(Tree root);
+void insert(Tree* root, int x);
+void delete(Tree* root, int x);
+void initialize(Tree* root);
+
+int main(){
+    Tree main;
+    initialize(&main);
+    insert(&main, 15);
+    insert(&main, 13);
+    insert(&main, 25);
+    insert(&main, 11);
+    insert(&main, 20);
+    insert(&main, 14);
+    insert(&main, 15);
+    delete(&main, 11);
+    display(main);
+
+}
+
+void display(Tree root){
+    for(int ndx = 0; ndx < MAX; ndx++){
+        printf("%d ", root.arr[ndx]);
+    }   
+}
+
+void initialize(Tree* root){
+    for(int ndx = 0; ndx < MAX; ndx++){
+        root->arr[ndx] = EMPTY;
+    }
+    root->count = 0;
+}
+
+void insert(Tree* root, int x){
+    int ndx = 0;
+    int lc = ndx * 2 + 1;
+    int rc = lc + 1;
+    while(ndx < MAX && root->arr[ndx] != EMPTY){
+        if(lc < MAX && x <= root->arr[ndx]){
+            ndx = lc;
+        }else if(rc < MAX){
+            ndx = rc;
+        }else{
+            break;
+        }
+        lc = ndx * 2 + 1;
+        rc = lc + 1;
+    }
+    
+    if(ndx < MAX){
+        root->count++;
+        root->arr[ndx] = x;
+    }
+}
+
+void delete(Tree* root, int x){
+    //find element
+    int ndx = 0;
+    int lc = ndx * 2 + 1;
+    int rc = lc + 1;
+    while(ndx < MAX && root->arr[ndx] != x && root->arr[ndx] != EMPTY){
+        if(lc < MAX && x <= root->arr[ndx]){
+            ndx = lc;
+        }else if(rc < MAX){
+            ndx = rc;
+        }else{
+            break;
+        }
+        lc = ndx * 2 + 1;
+        rc = lc + 1;
+    }
+
+    if(ndx < MAX){
+           
+    }
+}
---
+++ lastPassOrDie/finals/BST/linkedList.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+}*Tree;
+
+void display(Tree root);
+void insert(Tree* root, int x);
+void insertRecur(Tree* root, int x);
+void freeNodes(Tree* root);
+void delete(Tree* root, int x);
+Tree deleteRecur(Tree* root, int x);
+
+
+int main(){
+    Tree root = NULL;
+    insertRecur(&root, 15);
+    insertRecur(&root, 10);
+    insertRecur(&root, 20);
+    insertRecur(&root, 8);
+    insertRecur(&root, 12);
+    insertRecur(&root, 17);
+    insertRecur(&root, 25);
+    display(root);
+    printf("\n");
+    delete(&root, 15);
+    display(root);  
+}
+
+void freeNodes(Tree* root) {
+    if (*root == NULL) return;
+
+    freeNodes(&(*root)->left);
+    freeNodes(&(*root)->right);
+
+    free(*root);
+    *root = NULL; 
+}
+
+void display(Tree root){
+    if(root != NULL){
+        printf("%d ", root->data);
+        display(root->left);
+        display(root->right);
+    }
+}
+
+void insert(Tree* root, int x){
+    Tree* ptr = root;
+    while(*ptr != NULL){
+        ptr = (x <= (*ptr)->data) ? (&(*ptr)->left) : (&(*ptr)->right);
+    }
+
+    *ptr = (Tree)malloc(sizeof(struct node));
+    (*ptr)->data = x;
+    (*ptr)->left = NULL;
+    (*ptr)->right = NULL;
+}
+
+void insertRecur(Tree* root, int x){
+    if(*root == NULL){
+        Tree temp = (Tree)malloc(sizeof(struct node));
+        if(temp != NULL){
+            temp->data = x;
+            temp->left = temp->right = NULL;
+            *root = temp;
+        }
+    }else if(x <= (*root)->data){
+        insertRecur(&(*root)->left, x);
+    }else{
+        insertRecur(&(*root)->right, x);
+    }
+}
+
+void delete(Tree* root, int x){
+    Tree* ptr;
+    //find the element
+    for(ptr = root; *ptr != NULL && (*ptr)->data != x;){
+        ptr = (x <= (*ptr)->data) ? &(*ptr)->left : &(*ptr)->right; 
+    }
+
+    //if element is found check cases
+    /*
+    * CASE 1: node has no children, simply delete
+    * CASE 2: node has 1 children, change link and delete node
+    * CASE 3: node has 2 children, 
+    *  SOLUTIONS 
+    *   3.1: Find the minimum element in the right subtree of that node copy the value and delete the duplicate node.
+    *   3.2: Find the maximum element in the left subtree of that node, copy the value and delete the duplicate node.
+    *
+    */
+    if(*ptr != NULL){
+        Tree temp = *ptr;
+        if(temp->right != NULL && temp->left != NULL){
+            Tree *trav;
+            for(trav = &(*ptr)->right; *trav != NULL && (*trav)->left != NULL; trav = &(*trav)->left){}
+            temp->data = (*trav)->data; 
+            temp = *trav;
+            *trav = temp->right;
+        }else{
+            *ptr = temp->left == NULL ? temp->right : temp->left; 
+        }
+
+        free(temp);
+    }
+}
+
+// void deleteRecur(Tree* root, int x){
+//     traverse
+//     if()
+// }
+
+
+
